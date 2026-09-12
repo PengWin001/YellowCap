042b4d80f4baa0320f4373ffe9caf84e6213a5cf
PengWin001 <enoughalreadybruvv@gmail.com>
2026-09-12 11:38:43 +0530

    Minor Changes
    solved an error that didn't let the program run. There were two help commands, one in the code and one made by discord, so I had to disable the latter.

--- bot.py
+++ bot.py
@@ -10,6 +10,8 @@ TOKEN = os.getenv("DISCORD_TOKEN")
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix="!", intents=intents)
 
+bot.remove_command("help")
+
 COGS = [
     "cogs.moderation",
     "cogs.music",
